b3f61698941dabc738b6568ec76ccd22e0df85ff
Zack O'Keefe <67602855+zjokeefe@users.noreply.github.com>
2021-02-15 18:07:30 -0500

    Formatting and final function to form random message

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 // Create object to hold array of message options
 const options = {
-    _subject: ['You', 'I', 'We', 'He', 'She'],
+    _subject: ['You', 'I', 'We'],
     _verb: ['gave', 'have', 'made', 'took'],
     _poss: ['a', 'the'],
-    _noun: ['book', 'food', 'ball', 'movie'],
+    _noun: ['book', 'food', 'ball', 'movie', 'puzzle'],
 }
 
 // Function for random number
@@ -12,9 +12,15 @@ const random = num => {
 }
 
 // Loop through object to make message
-let message = [];
-
-for (const property in options) {
-    let index = random(options[property].length);
-    message.push(options[property][index]);
+const randomMessage = () => { 
+    let message = [];
+    for (const property in options) {
+        let index = random(options[property].length);
+        message.push(options[property][index]);
+    }
+    // Format the output
+    console.log(message.join(' ') + '.');
 }
+
+// Run program to get random message
+randomMessage();
